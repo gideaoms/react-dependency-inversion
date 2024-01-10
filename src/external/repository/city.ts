6c7c29b23ec0bb83f3ab5155cc549db18dfe7d4b
Gideão <gideaoms@gmail.com>
2024-01-10 17:50:41 -0300

    add comment

--- src/external/repository/city.ts
+++ src/external/repository/city.ts
@@ -2,6 +2,7 @@ import { CityRepository as ICityRepository } from "../../core/repository/city";
 
 export class CityRepository implements ICityRepository {
   async findMany() {
+    // here you could call an api using axios
     return [
       { id: "1", name: "City 1" },
       { id: "2", name: "City 2" },
